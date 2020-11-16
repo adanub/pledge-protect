9ad61dc6ccbc040296c259eaf28cbc67807b67ad
Raunak <raunaksitoula@icloud.com>
2020-11-17 02:50:49 +1100

    See description
    - Made map slightly (very slightly) darker to improve readability
    - Added floating menu to home page
    - Added fonts
    - Added app logo
    - Added dropdown button (not functional yet)
    - Added about button

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,36 @@
 <template>
   <div class="container">
-    <div id="map">
-       
+    <div id="map"></div>
+    <div class="content-box">
+      <app-logo class="app-logo"/>
+      <h1 class="title"><b class="pledge-pink">Pledge</b> & <b class="pledge-pink">Protect</b></h1>
+      <dropdown class="dropdown-button"/>
+      <button class="about-button">What's this about?</button>
     </div>
   </div>
 </template>
 
 <script>
+import Dropdown from '~/components/Dropdown.vue'
+import AppLogo from '~/components/Logo.vue'
 
 export default {
+  components: {
+    Dropdown,
+    AppLogo
+  },
+
   data: () => ({
     map: null
   }),
   methods: {
+    //Sets up and starts loading the interactive map
     InitialiseMap() {
       var mapOptions = {
         center: [31.2532, 146.9211],
         zoom: 16,
-        zoomControl: true
+        zoomControl: true,
+        attributionControl: false
       }
 
       this.map = this.$L.map('map', mapOptions).fitWorld();
@@ -34,18 +47,58 @@ export default {
     }
   },
   mounted() {
-    setTimeout(this.InitialiseMap(), 50);
+    setTimeout(this.InitialiseMap(), 50); //calls InitialiseMap() 50ms after the page finishes loading
   }
 }
-
 </script>
 
 <style>
+.app-logo {
+  position: absolute;
+  left: 16px;
+  top: 8px;
+}
+
 #map {
   position: absolute;
   height: 100%;
   width: 100vw;
   z-index: 2;
+
+  animation: 2s fade-in;
+}
+
+.about-button {
+  background-color: #f5f5f5;
+  border-width: 2px;
+  border-radius: 8px;
+  border-color: rgba(59, 65, 60, 0.7);
+
+  color: rgba(59, 65, 60, 0.7);
+  font-family: 'Kalam', cursive;
+  font-size: 18px;
+  padding-top: 6px;
+
+  margin-top: 16px;
+  height: 44px;
+  min-width: 288px;
+  width: 90%;
+
+  transition: 0.1s;
+}
+
+.dropdown-button {
+  position: absolute;
+  right: 8px;
+  top: 8px;
+}
+
+.about-button:hover {
+  background-color: #3B413C;
+  color: #f5f5f5;
+
+  letter-spacing: 0.05rem;
+  transition: 0.2s;
 }
 
 .container {
@@ -55,6 +108,26 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  font-family: 'Heebo', sans-serif;
+  color: #3B413C;
+
+  animation: 1s fade-in;
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+}
+
+.title {
+  font-family: 'Kalam', cursive;
+  font-size: 24px;
+  vertical-align: text-top;
+}
+
+.pledge-pink {
+  color: #F34A63;
 }
 
 .links {
@@ -63,9 +136,16 @@ export default {
 
 .content-box {
   display: block;
-  background-color: #f2f2f2;
+  position: absolute;
+  background-color: #f5f5f5;
   width: 90vw;
-  min-height: 300px;
+  min-height: 200px;
+  min-width: 300px;
+  z-index: 3;
+  border-radius: 16px;
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.25);
+  top: 16px;
+  padding: 18px;
 }
 
 @screen sm {
