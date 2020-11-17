feb016af38edfc509f55e16a46dd0eef1e68f210
Raunak <raunaksitoula@icloud.com>
2020-11-17 14:19:39 +1100

    Added pledge/protect buttons

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,10 @@
       </h1>
       <button  class="dropdown-button" onclick="this.blur();"><dropdown/></button> <!-- Note: onclick="this.blur();" removes focused/selected state after it is pressed, gets rid of the aesthetic issue without compromising accessibility -->
       <button class="about-button" onclick="this.blur();">What's this about?</button>
+      <div class="button-container">
+        <div class="pledge-button-base float-left"><button class="pledge-button" onclick="this.blur();"><pledge-icon class="pledge-button-icon"/><div class="inline-block">Pledge</div></button></div>
+        <div class="protect-button-base float-right"><button class="protect-button" onclick="this.blur();"><protect-icon class="protect-button-icon"/><div class="inline-block">Protect</div></button></div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,11 +19,15 @@
 <script>
 import Dropdown from '~/components/Dropdown.vue'
 import AppLogo from '~/components/Logo.vue'
+import ProtectIcon from '~/components/ProtectIcon.vue'
+import PledgeIcon from '~/components/PledgeIcon.vue'
 
 export default {
   components: {
     Dropdown,
-    AppLogo
+    AppLogo,
+    ProtectIcon,
+    PledgeIcon
   },
 
   data: () => ({
@@ -55,6 +63,19 @@ export default {
 </script>
 
 <style>
+.pledge-button-icon {
+  position: absolute;
+  margin-left: auto;
+  margin-top: -2px;
+}
+
+.protect-button-icon {
+  position: absolute;
+  margin-left: auto;
+  margin-right: ;
+  margin-top: -2px;
+}
+
 .app-logo {
   position: absolute;
   left: 16px;
@@ -72,6 +93,7 @@ export default {
 
 .about-button {
   background-color: #f5f5f5;
+
   border-width: 2px;
   border-radius: 8px;
   border-color: rgba(59, 65, 60, 0.7);
@@ -84,7 +106,7 @@ export default {
   margin-top: 16px;
   height: 44px;
   min-width: 288px;
-  width: 90%;
+  width: 84%;
 
   transition: 0.1s;
 }
@@ -100,6 +122,92 @@ export default {
   background-color: rgba(59, 65, 60, 0.9);
   letter-spacing: -0.01rem;
   transition: 0.1s;
+}
+
+.pledge-button-base {
+  background-color: #D64157;
+  border-radius: 8px;
+
+  margin-top: 16px;
+  margin-left: auto;
+  margin-right: auto;
+  height: 44px;
+  min-width: 135px;
+  width: 46.875%;
+}
+.pledge-button {
+  background-color: #F34A63;
+  border-radius: 8px;
+
+  color: white;
+  font-family: 'Kalam', cursive;
+  font-size: 18px;
+  padding-top: 6px;
+  padding-left: 24px;
+
+  margin-top: -4px;
+  height: 44px;
+  min-width: 135px;
+  width: 100%;
+
+  transition: 0.2s;
+}
+.pledge-button:hover, .pledge-button:focus {
+  outline: none;
+  margin-top: -7px;
+
+  transition: 0.1s;
+}
+.pledge-button:active {
+  margin-top: 0px;
+  transition: 0.1s;
+}
+
+.protect-button-base {
+  background-color: #0C8A75;
+  border-radius: 8px;
+
+  margin-top: 16px;
+  margin-left: auto;
+  margin-right: auto;
+  height: 44px;
+  min-width: 135px;
+  width: 46.875%;
+}
+.protect-button {
+  background-color: #0FA78E;
+  border-radius: 8px;
+
+  color: white;
+  font-family: 'Kalam', cursive;
+  font-size: 18px;
+  padding-top: 6px;
+  padding-left: 24px;
+
+  margin-top: -4px;
+  height: 44px;
+  min-width: 135px;
+  width: 100%;
+
+  transition: 0.2s;
+}
+.protect-button:hover, .protect-button:focus {
+  outline: none;
+  margin-top: -7px;
+
+  transition: 0.1s;
+}
+.protect-button:active {
+  margin-top: 0px;
+  transition: 0.1s;
+}
+
+.button-container {
+  margin: auto;
+  height: 60px;
+  vertical-align: bottom;
+  min-width: 288px;
+  width: 84%;
 }
 
 .dropdown-button {
@@ -159,6 +267,7 @@ export default {
   border-radius: 16px;
   box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.25);
   top: 16px;
-  padding: 18px;
+  padding-top: 18px;
+  padding-bottom: 18px;
 }
 </style>
