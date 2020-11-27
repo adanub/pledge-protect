d99e897a00e43b2a051a58a219b2b5d2d01896be
Raunak <raunaksitoula@icloud.com>
2020-11-27 16:18:16 +1100

    Pins now evenly spread out without overlapping, and do not spawn far out to sea

--- pages/index.vue
+++ pages/index.vue
@@ -28,9 +28,6 @@ import AppLogo from '~/components/Logo.vue'
 import ProtectIcon from '~/components/ProtectIcon.vue'
 import PledgeIcon from '~/components/PledgeIcon.vue'
 
-import ProtectPin from '~/components/ProtectPin.vue'
-import PledgePin from '~/components/PledgePin.vue'
-
 import Vue from 'vue'
 
 export default {
@@ -38,15 +35,12 @@ export default {
     Dropdown,
     AppLogo,
     ProtectIcon,
-    PledgeIcon,
-    PledgePin,
-    ProtectPin
+    PledgeIcon
   },
 
   data: () => ({
     map: null,
-    pledgePins: new Array(3),
-    protectPins: new Array(3)
+    pinMarkers: new Array(6)
   }),
   methods: {
     //Sets up and starts loading the interactive map
@@ -81,14 +75,16 @@ export default {
     //Generates 3 pledge pins, and 3 protect pins at random locations on the screen
     GeneratePins() {
       var mapBounds = this.map.getBounds();
-      console.log("Map bounds x: " + mapBounds.getEast() + " || " + mapBounds.getWest());
-      console.log("Map bounds y: " + mapBounds.getNorth() + " || " + mapBounds.getSouth());
 
       //Pledge Pins
       for (var i = 0; i < 3; i++) {
         //Randomised longitude within view
         var lngMagnitude = mapBounds.getEast() - mapBounds.getWest();
-        var lng = (Math.random() * lngMagnitude * 0.8) + mapBounds.getWest() + lngMagnitude * 0.1;
+        var magnitudeMultiplier = 1.0; //Going to use this to half magnitude to left side of screen if sea is visible
+        if (mapBounds.getEast() > 151.352562) {
+          magnitudeMultiplier = 0.5;
+        }
+        var lng = (Math.random() * lngMagnitude * 0.26 * magnitudeMultiplier) + mapBounds.getWest() + lngMagnitude * 0.1 + (lngMagnitude * 0.26 * i * magnitudeMultiplier);
 
         //Randomised latitude within view
         var latMagnitude = mapBounds.getNorth() - mapBounds.getSouth();
@@ -99,7 +95,6 @@ export default {
     },
 
     CreatePledgePin(pinId,  lat, lng) {
-
       //Creating custom marker
       var pledgeIcon = this.$L.divIcon({
         iconSize: null,
@@ -107,10 +102,24 @@ export default {
       });
       
       var marker = this.$L.marker([lat, lng], {icon: pledgeIcon}).addTo(this.map);
-      if (this.pledgePins[pinId] != null) {
-        this.map.removeLayer(this.pledgePins[pinId]);
+      if (this.pinMarkers[pinId] != null) {
+        this.map.removeLayer(this.pinMarkers[pinId]);
       }
-      this.pledgePins[pinId] = marker;
+      this.pinMarkers[pinId] = marker;
+    },
+
+    CreateProtectPin(pinId,  lat, lng) {
+      //Creating custom marker
+      var protectIcon = this.$L.divIcon({
+        iconSize: null,
+        html: '<a href="" class="pin-marker"><svg width="65" height="86" viewBox="0 0 65 86" fill="none" xmlns="http://www.w3.org/2000/svg"> <g filter="url(#filter0_d)"> <path d="M32.0416 52.0577C45.3123 52.0577 56.0704 41.2996 56.0704 28.0289C56.0704 14.7581 45.3123 4 32.0416 4C18.7708 4 8.0127 14.7581 8.0127 28.0289C8.0127 41.2996 18.7708 52.0577 32.0416 52.0577Z" fill="#0FA78E"/> <path fill-rule="evenodd" clip-rule="evenodd" d="M32.0417 73.9916C32.0417 73.9916 7.35805 43.9873 8.0128 28.0294C8.26887 21.7873 56.1584 22.4045 56.0705 27.6867C55.7432 47.3719 32.0417 73.9916 32.0417 73.9916Z" fill="#0FA78E"/> <g clip-path="url(#clip0)"> <path fill-rule="evenodd" clip-rule="evenodd" d="M32.4746 17.0051L46.8785 22.0192C46.8785 22.0192 46.4073 32.0672 44.232 36.3979C40.2609 44.3039 32.8499 45.8587 32.4746 45.9329V45.9358L32.4671 45.9343L32.4597 45.9358V45.9329C32.0843 45.8587 24.6735 44.3039 20.7022 36.3979C18.5269 32.0672 18.0557 22.0192 18.0557 22.0192L32.4597 17.0051V17L32.4671 17.0026L32.4746 17V17.0051ZM40.4378 38.701H24.4966C24.4966 34.302 28.068 30.7305 32.4671 30.7305C36.8662 30.7305 40.4378 34.302 40.4378 38.701ZM32.4671 22.0084C34.5474 22.0084 36.2363 23.6973 36.2363 25.7775C36.2363 27.8578 34.5474 29.5467 32.4671 29.5467C30.3868 29.5467 28.698 27.8578 28.698 25.7775C28.698 23.6973 30.3868 22.0084 32.4671 22.0084Z" fill="white"/> </g> </g> <defs> <filter id="filter0_d" x="0" y="0" width="64.0707" height="85.9916" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"> <feFlood flood-opacity="0" result="BackgroundImageFix"/> <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/> <feOffset dy="4"/> <feGaussianBlur stdDeviation="4"/> <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/> <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/> <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/> </filter> <clipPath id="clip0"> <rect width="29" height="29" fill="white" transform="translate(18 17)"/> </clipPath> </defs> </svg></a>'
+      });
+      
+      var marker = this.$L.marker([lat, lng], {icon: pledgeIcon}).addTo(this.map);
+      if (this.pinMarkers[pinId] != null) {
+        this.map.removeLayer(this.pinMarkers[pinId]);
+      }
+      this.pinMarkers[pinId] = marker;
     }
   },
   mounted() {
