ed1e8da1130be54b9270dde71a5d700fdd5ea1a9
Raunak <raunaksitoula@icloud.com>
2020-11-27 20:22:24 +1100

    Added bouncy scale animations to pledge/protect pins on generation and hover

--- components/ProtectPin.vue
+++ components/ProtectPin.vue
@@ -1,28 +1,5 @@
 <template>
-    <svg width="65" height="86" viewBox="0 0 65 86" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g filter="url(#filter0_d)">
-        <path d="M32.0416 52.0577C45.3123 52.0577 56.0704 41.2996 56.0704 28.0289C56.0704 14.7581 45.3123 4 32.0416 4C18.7708 4 8.0127 14.7581 8.0127 28.0289C8.0127 41.2996 18.7708 52.0577 32.0416 52.0577Z" fill="#0FA78E"/>
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M32.0417 73.9916C32.0417 73.9916 7.35805 43.9873 8.0128 28.0294C8.26887 21.7873 56.1584 22.4045 56.0705 27.6867C55.7432 47.3719 32.0417 73.9916 32.0417 73.9916Z" fill="#0FA78E"/>
-        <g clip-path="url(#clip0)">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M32.4746 17.0051L46.8785 22.0192C46.8785 22.0192 46.4073 32.0672 44.232 36.3979C40.2609 44.3039 32.8499 45.8587 32.4746 45.9329V45.9358L32.4671 45.9343L32.4597 45.9358V45.9329C32.0843 45.8587 24.6735 44.3039 20.7022 36.3979C18.5269 32.0672 18.0557 22.0192 18.0557 22.0192L32.4597 17.0051V17L32.4671 17.0026L32.4746 17V17.0051ZM40.4378 38.701H24.4966C24.4966 34.302 28.068 30.7305 32.4671 30.7305C36.8662 30.7305 40.4378 34.302 40.4378 38.701ZM32.4671 22.0084C34.5474 22.0084 36.2363 23.6973 36.2363 25.7775C36.2363 27.8578 34.5474 29.5467 32.4671 29.5467C30.3868 29.5467 28.698 27.8578 28.698 25.7775C28.698 23.6973 30.3868 22.0084 32.4671 22.0084Z" fill="white"/>
-        </g>
-        </g>
-        <defs>
-        <filter id="filter0_d" x="0" y="0" width="64.0707" height="85.9916" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
-        <feOffset dy="4"/>
-        <feGaussianBlur stdDeviation="4"/>
-        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
-        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
-        </filter>
-        <clipPath id="clip0">
-        <rect width="29" height="29" fill="white" transform="translate(18 17)"/>
-        </clipPath>
-        </defs>
-    </svg>
-
+    <svg width="65" height="86" fill="none" xmlns="http://www.w3.org/2000/svg"><g filter="url(#filter0_d)"><path d="M32.042 52.058c13.27 0 24.028-10.758 24.028-24.03C56.07 14.759 45.312 4 32.042 4 18.77 4 8.012 14.758 8.012 28.029c0 13.27 10.759 24.029 24.03 24.029z" fill="#0FA78E"/><path fill-rule="evenodd" clip-rule="evenodd" d="M32.042 73.992S7.358 43.987 8.012 28.029c.257-6.242 48.146-5.625 48.059-.342-.328 19.685-24.03 46.305-24.03 46.305z" fill="#0FA78E"/><g clip-path="url(#clip0)"><path fill-rule="evenodd" clip-rule="evenodd" d="M32.475 17.005l14.404 5.014s-.472 10.048-2.647 14.379c-3.971 7.906-11.382 9.46-11.757 9.535v.003l-.008-.002-.007.002v-.003c-.376-.074-7.787-1.63-11.758-9.535-2.175-4.33-2.646-14.379-2.646-14.379l14.404-5.014V17l.007.003.008-.003v.005zm7.963 21.696H24.497a7.974 7.974 0 017.97-7.97c4.4 0 7.97 3.571 7.97 7.97zm-7.97-16.693a3.77 3.77 0 013.768 3.77c0 2.08-1.689 3.769-3.769 3.769a3.77 3.77 0 01-3.769-3.77 3.77 3.77 0 013.77-3.769z" fill="#fff"/></g></g><defs><clipPath id="clip0"><path fill="#fff" transform="translate(18 17)" d="M0 0h29v29H0z"/></clipPath><filter id="filter0_d" x="0" y="0" width="64.071" height="85.992" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="4"/><feGaussianBlur stdDeviation="4"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>
 </template>
 
 <style scoped>
