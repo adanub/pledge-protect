b3807e46eb0f6881e4324418e6b89eac1d90619e
Griffen Edge <griffedge@users.noreply.github.com>
2020-11-29 15:19:01 +1100

    Change app title to "Pledge & Protect"
    Changes the app title from "pledge-protect" to "Pledge & Protect"

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'pledge-protect',
+    title: 'Pledge &amp; Protect',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
