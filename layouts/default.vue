eb87f0e2606469cf70222cce752d6dee735f0d6b
Raunak <raunaksitoula@icloud.com>
2020-11-29 19:52:47 +1100

    Changed default text selection colour for compatible browsers

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,6 +26,14 @@ html {
   background-color: #EFF0F0;
 }
 
+/*Adjusting selection highlight colours*/
+::selection {
+  background: rgba(113, 133, 236, 0.23); /* WebKit/Blink Browsers */
+}
+::-moz-selection {
+  background: rgba(113, 133, 236, 0.23); /* Gecko Browsers */
+}
+
 *,
 *::before,
 *::after {
@@ -33,32 +41,5 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
